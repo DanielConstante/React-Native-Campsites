bd0949eaea8713064922b61d8c6e68fb57ac1213
Daniel Constante <daniel_constante86@hotmail.com>
2021-08-03 12:05:36 -0500

    Animatable

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -4,12 +4,13 @@ import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
         campsites: state.campsites
     };
-}; 
+};
 
 class Directory extends Component {
 
@@ -23,13 +24,15 @@ class Directory extends Component {
 
         const renderDirectoryItem = ({ item }) => {
             return (
-                <Tile
-                    title={item.name}
-                    caption={item.description}
-                    featured
-                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id})}
-                    imageSrc={{uri: baseUrl + item.image}}
-                />
+                <Animatable.View animation='fadeInRightBig' duration={2000}>
+                    <Tile
+                        title={item.name}
+                        caption={item.description}
+                        featured
+                        onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                        imageSrc={{ uri: baseUrl + item.image }}
+                    />
+                </Animatable.View>
             );
         };
 
@@ -53,4 +56,4 @@ class Directory extends Component {
     }
 }
 
-export default connect(mapStateToProps) (Directory);
+export default connect(mapStateToProps)(Directory);
